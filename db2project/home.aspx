49555839376dec6f8852a1ea08351de55123ebec
bhavishyachandra <bhavishyachandra@gmail.com>
2015-04-18 03:37:24 -0400

    v1.0

---
+++ db2project/home.aspx
@@ -0,0 +1,114 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="home.aspx.cs" Inherits="db2project.home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="Content/bootstrap.min.css"/>
+    <!-- Optional theme -->
+<link rel="stylesheet" href="Content/lavish-bootstrap.min.css" type="text/css"/>
+
+<!-- jQuery library -->
+<script src="Scripts/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="~/Scripts/bootstrap.min.js"></script>
+    <title></title>
+
+</head>
+<body>
+
+    <form id="form1" runat="server">
+
+    <div>
+        <div class="container">
+	<div class="row clearfix">
+		<div class="col-md-12 column">
+			&nbsp;<br />
+            <br />
+            <br />
+            <br />
+			<img src="Images/imdb_thumbnail.jpg" class="img-circle" /><div class="page-header">
+				<h1>
+					IMDB Using MongoDB
+				</h1>
+			</div>
+			<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+				<div class="navbar-header">
+					 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button> 
+				</div>
+				
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav">
+						<li class="active">
+							<a href="http://www.imdb.com/">IMDB</a>
+						</li>
+						<li>
+							<a href="http://www.mongodb.org/">MongoDB</a>
+						</li>
+						<li class="dropdown">
+							 <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown<strong class="caret"></strong></a>
+							<ul class="dropdown-menu">
+								<li>
+									<a href="#">Action</a>
+								</li>
+								<li>
+									<a href="#">Another action</a>
+								</li>
+								<li>
+									<a href="#">Something else here</a>
+								</li>
+								<li class="divider">
+								</li>
+								<li>
+									<a href="#">Separated link</a>
+								</li>
+								<li class="divider">
+								</li>
+								<li>
+									<a href="#">One more separated link</a>
+								</li>
+							</ul>
+						</li>
+					</ul>
+
+					<ul class="nav navbar-nav navbar-right">
+						<li>
+							<a href="#">Link</a>
+						</li>
+						<li class="dropdown">
+							 <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown<strong class="caret"></strong></a>
+							<ul class="dropdown-menu">
+								<li>
+									<a href="#">Action</a>
+								</li>
+								<li>
+									<a href="#">Another action</a>
+								</li>
+								<li>
+									<a href="#">Something else here</a>
+								</li>
+								<li class="divider">
+								</li>
+								<li>
+									<a href="#">Separated link</a>
+								</li>
+							</ul>
+						</li>
+					</ul>
+				</div>
+				
+			</nav> <asp:Button ID="MoviesInRange" runat="server" OnClick="MoviesInRange_Click" Text="Movies From 2013 to 2015" class="btn btn-info"/> <asp:Button ID="TopRatedMovies" runat="server" Text="Top Rated Movies" OnClick="TopRatedMovies_Click" class="btn btn-info"/> <asp:Button ID="MoviesOf2013" runat="server" OnClick="MoviesOf2013_Click" Text="Movies of 2013" class="btn btn-info"/>
+			<asp:GridView ID="ResultGridView" runat="server" class="table table-hover table-bordered">
+				
+			</asp:GridView>
+		</div>
+	</div>
+</div>
+    
+    </div>
+    </form>
+
+</body>
+</html>
---
+++ db2project/home.aspx.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Linq;
+using MongoDB.Driver.Builders;
+using System.Data;
+
+
+
+namespace db2project
+{
+    public partial class home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+
+
+
+
+
+
+        }
+
+        public class City
+        {
+            public string _id { get; set; }
+            public string name { get; set; }
+        }
+
+        protected void InsertButton_Click(object sender, EventArgs e)
+        {
+            //var connectionString = "mongodb://localhost:27017/";
+            //var client = new MongoClient(connectionString);
+            //var server = client.GetServer();
+            //MongoDatabase db = server.GetDatabase("test_bhavishya");
+            //MongoCollection collection = db.GetCollection<BsonDocument>("test_collection_bhavishya");
+            //BsonDocument city = new BsonDocument {
+            //    { "_id", InsertIDTextBox.Text.ToString() },
+            //    { "name", InsertNameTextBox.Text.ToString() }
+            //    };
+            //Response.Write("Bson created" + city.ToJson());
+            //collection.Insert(city);
+            //City city1 = new City { _id = "12", name = "vij" };
+            //Response.Write("insert succesful");
+
+
+
+
+
+
+
+
+        }
+
+        protected void TopRatedMovies_Click(object sender, EventArgs e)
+        {
+
+            var connectionString = "mongodb://35.16.0.75:27017/";
+
+            var client = new MongoClient(connectionString);
+            var server = client.GetServer();
+
+            MongoDatabase db = server.GetDatabase("imdb");
+            MongoCollection<BsonDocument> collection = db.GetCollection<BsonDocument>("ratings");
+            //var query = Query.EQ("_id", findIDTextBoxText);
+
+            //MongoCursor<BsonDocument> cursor = collection.Find(query);
+
+            var sort1 = SortBy.Descending("votes").Descending("rank");
+
+            MongoCursor<BsonDocument> cursor1 = collection.FindAll().SetSortOrder(sort1).SetLimit(50);
+
+            cursor1.SetFields(Fields.Include("title", "rank", "votes"));
+            //ResultTextBox.Text += cursor1.ToJson();
+            DataTable dt = new DataTable();
+            dt.Columns.Add("title");
+            dt.Columns.Add("rank");
+            dt.Columns.Add("votes");
+            foreach (var i in cursor1)
+            {
+                dt.Rows.Add(i["title"], i["rank"], i["votes"]);
+            }
+
+
+            dt.DefaultView.Sort = "rank DESC";
+
+            ResultGridView.DataSource = dt;
+            ResultGridView.DataBind();
+
+        }
+
+        protected void MoviesOf2013_Click(object sender, EventArgs e)
+        {
+            var connectionString = "mongodb://192.168.1.134:27017/";
+            var client = new MongoClient(connectionString);
+            var server = client.GetServer();
+
+
+            MongoDatabase db = server.GetDatabase("imdb");
+            MongoCollection<BsonDocument> collection = db.GetCollection<BsonDocument>("movies");
+            var query = Query.And(Query.EQ("year", 2013), Query.EQ("type", "(MOVIE)"));
+            MongoCursor<BsonDocument> cursor = collection.Find(query).SetLimit(50);
+            cursor.SetFields(Fields.Include("title"));
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("title");
+
+            foreach (var i in cursor)
+            {
+                dt.Rows.Add(i["title"]);
+            }
+
+            ResultGridView.DataSource = dt;
+            ResultGridView.DataBind();
+        }
+
+        protected void ResultTextBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void MoviesInRange_Click(object sender, EventArgs e)
+        {
+            var connectionString = "mongodb://192.168.1.134:27017/";
+            var client = new MongoClient(connectionString);
+            var server = client.GetServer();
+
+
+            MongoDatabase db = server.GetDatabase("imdb");
+            MongoCollection<BsonDocument> collection = db.GetCollection<BsonDocument>("movies");
+            var query = Query.And(Query.EQ("type", "(MOVIE)"), Query.LTE("year", 2015), Query.GTE("year", 2013));
+            MongoCursor<BsonDocument> cursor = collection.Find(query).SetLimit(50);
+            cursor.SetFields(Fields.Include("title"));
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("title");
+
+            foreach (var i in cursor)
+            {
+                dt.Rows.Add(i["title"]);
+            }
+
+            ResultGridView.DataSource = dt;
+            ResultGridView.DataBind();
+
+
+        }
+
+
+    }
+}
